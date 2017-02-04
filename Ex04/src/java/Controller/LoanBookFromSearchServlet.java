01b24d1f4240d766064a908f758916e4f66eb4e9
masmddr <masmddr@gmail.com>
2017-02-04 16:12:02 +0200

    Botched together the logic of LoanBookFromSearchServlet.

--- Ex04/src/java/Controller/LoanBookFromSearchServlet.java
+++ Ex04/src/java/Controller/LoanBookFromSearchServlet.java
@@ -1,9 +1,12 @@
 package Controller;
 
 import DB.CookieDB;
+import DB.LibraryPropsDB;
 import DB.LoanDB;
+import DB.StudentDB;
 import DB.UserDB;
 import Model.Loan;
+import Model.Student;
 import Model.User;
 import java.io.IOException;
 import java.sql.Connection;
@@ -54,74 +57,108 @@ public class LoanBookFromSearchServlet extends HttpServlet{
 	 */// </editor-fold>
 	protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
 		/*
-		*
-		*
-		* TODO
-		* Remember to test this Servlet
-		*
-		*
-		*
+		 *
+		 *
+		 * TODO
+		 * Remember to test this Servlet
+		 *
+		 *
+		 *
 		 */
+
 		Connection cn;
 		RequestDispatcher rd;
 		Cookie currentUser = CookieDB.getCookieValue(request.getCookies(), "username");
+
 		UserDB myUserDb;
+		LoanDB lnDB;
+		LibraryPropsDB lpDB;
+		StudentDB sDB;
+
 		User crrUser;
+		Student st = null;
+
 		String bookISBN = request.getParameter("bookIsbn");
 		String userID = request.getParameter("userId");
+		String stID = request.getParameter("stId");
+
 		try{
 			cn = DriverManager.getConnection(this.sc.getInitParameter("cnurl"), this.sc.getInitParameter("DBUsername"), this.sc.getInitParameter("DBPassword"));
 			myUserDb = new UserDB(cn);
 
-			if(bookISBN == null || currentUser == null){
+			if(bookISBN == null || currentUser == null || (crrUser = myUserDb.getUser(currentUser.getValue())) == null){
 				rd = request.getRequestDispatcher("SearchBookServlet");
 				rd.forward(request, response);
 				return;
 			}
 
-			crrUser = myUserDb.getUser(currentUser.getValue());
-			if(crrUser.getUserID().equals("admin") || userID == null){
-				rd = request.getRequestDispatcher("IdentifyUserServlet"); //TODO Write this page/servlet
-				rd.forward(request, response);
-				return;
+			sDB = new StudentDB(cn);
+			if(stID == null){
+				if(crrUser.getUserType().equals("user")){
+					st = sDB.getStudent(crrUser.getUserID());
+					stID = st.getStudentID();
+				} else if(crrUser.getUserID().equals("admin")){
+					rd = request.getRequestDispatcher("IdentifyUserServlet"); //TODO Write this page/servlet
+					rd.forward(request, response);
+					return;
+				}
 			}
 
-			LoanDB lnDB = new LoanDB(cn);
+			if(st == null){
+				st = sDB.getStudent(crrUser.getUserID());
+			}
+
+			lnDB = new LoanDB(cn);
 			GregorianCalendar today = new GregorianCalendar();
 			GregorianCalendar returnBy = new GregorianCalendar();
 			returnBy.add(GregorianCalendar.DAY_OF_MONTH, 14);
-			Loan newLoan = lnDB.getNewLoan(userID, today, returnBy);
+			Loan newLoan = lnDB.getNewLoan(stID, today, returnBy);
 
 			if(newLoan == null){
 				// TODO
 				// Redirect to an error page saying something bad has happned.
 				/*
-				rd = request.getRequestDispatcher("SearchBookServlet");
-				rd.forward(request, response);
+				 * rd = request.getRequestDispatcher("SearchBookServlet");
+				 * rd.forward(request, response);
 				 */
 				return;
 			}
 
-			/*
-			TODO
-			Check user fines and number of currently loaned books.
-			 */
+			lpDB = new LibraryPropsDB(cn);
+			int maxFinesPerStudent = lpDB.getMaxFinesPerStudent();
+			int maxBooksPerStudent = lpDB.getMaxBooksPerStudent();
+
+			if(st.getCurrentFines() >= maxFinesPerStudent){
+				/*
+				 * TODO
+				 * Redirect to error page about fines.
+				 */
+				return;
+			}
+			if(sDB.getCountLoanedBooks(stID) >= maxBooksPerStudent){
+				/*
+				 * TODO
+				 * Redirect to error page about book count.
+				 */
+				return;
+			}
+
 			if(lnDB.addBookToLoan(newLoan.getLoanID(), bookISBN)){
 				// TODO
 				// Redirect to a page that handles loans.
 				/*
-				rd = request.getRequestDispatcher("");
-				request.setAttribute("loanID", newLoan.getLoanID());
-				request.setAttribute("studentID", userID);
-				rd.forward(request, response);
+				 * rd = request.getRequestDispatcher("");
+				 * request.setAttribute("loanID", newLoan.getLoanID());
+				 * request.setAttribute("studentID", userID);
+				 * rd.forward(request, response);
 				 */
 				return;
 			} else {
 				// TODO
 				// Redirect to an error page saying something bad has happned.
 				/*
-				rd = request.getRequestDispatcher("SearchBookServlet");
-				rd.forward(request, response);
+				 * rd = request.getRequestDispatcher("SearchBookServlet");
+				 * rd.forward(request, response);
 				 */
 				return;
 			}

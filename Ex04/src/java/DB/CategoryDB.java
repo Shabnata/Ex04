490725a70fe9210c059418418d074a5790471d20
masmddr <masmddr@gmail.com>
2017-02-10 18:51:39 +0200

    Changed getBooksByCategoryName() to correctly set Book's availableCopies count.

--- Ex04/src/java/DB/CategoryDB.java
+++ Ex04/src/java/DB/CategoryDB.java
@@ -1,3 +1,4 @@
+
 package DB;
 
 import Model.*;
@@ -11,18 +12,18 @@ import java.time.Year;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
-public class CategoryDB {
+public class CategoryDB{
 
 	private Connection cn;
 
-	public CategoryDB(Connection cn) {
+	public CategoryDB(Connection cn){
 		this.cn = cn;
 	}
 
-	public CategoryDB() {
+	public CategoryDB(){
 	}
 
-	public Category getCategory(String catName) throws ClassNotFoundException, SQLException {
+	public Category getCategory(String catName) throws ClassNotFoundException, SQLException{
 		Class.forName("org.apache.derby.jdbc.ClientDriver");
 		String urlCn = "jdbc:derby://localhost:1527/LibraryDB";
 		Connection cn = DriverManager.getConnection(urlCn, "administrator", "123456");
@@ -36,20 +37,20 @@ public class CategoryDB {
 			ps = cn.createStatement();
 
 			ResultSet rs = ps.executeQuery("SELECT * FROM categories WHERE cat_name=\'" + catName + "\'");
-			while (rs.next()) {
+			while(rs.next()){
 				ct = new Category();
 				ct.setCatID(rs.getInt("id"));
 				ct.setCatName(rs.getString("cat_name"));
 			}
 			cn.close();
-		} catch (SQLException e) {
+		} catch(SQLException e){
 			// TODO
 			// Write an error
 		}
 		return ct;
 	}
 
-	public ArrayList<Category> getCategories() throws ClassNotFoundException, SQLException {
+	public ArrayList<Category> getCategories() throws ClassNotFoundException, SQLException{
 		Class.forName("org.apache.derby.jdbc.ClientDriver");
 		String urlCn = "jdbc:derby://localhost:1527/LibraryDB";
 		Connection cn = DriverManager.getConnection(urlCn, "administrator", "123456");
@@ -62,21 +63,21 @@ public class CategoryDB {
 
 			ResultSet rs = ps.executeQuery("select * from Categories");
 
-			while (rs.next()) {
+			while(rs.next()){
 				Category c = new Category();
 				c.setCatID(rs.getInt(1));
 				c.setCatName(rs.getString(2));
 				cat.add(c);
 			}
 			cn.close();
-		} catch (SQLException e) {
+		} catch(SQLException e){
 			// TODO
 			// Write an error
 		}
 		return cat;
 	}
 
-	public Boolean addCategory(String catName) throws ClassNotFoundException, SQLException {
+	public Boolean addCategory(String catName) throws ClassNotFoundException, SQLException{
 		Boolean added = false;
 		Class.forName("org.apache.derby.jdbc.ClientDriver");
 		String urlCn = "jdbc:derby://localhost:1527/LibraryDB";
@@ -90,31 +91,55 @@ public class CategoryDB {
 			pst.setString(1, catName);
 
 			int nor = pst.executeUpdate();
-			if (nor != 0) {
+			if(nor != 0){
 				added = true;
 			}
 
 			cn.close();
-		} catch (SQLException e) {
+		} catch(SQLException e){
 			// TODO
 			// Write an error
 		}
 		return added;
 	}
 
-	public ArrayList<Book> getBooksByCategoryName(String catName) throws ClassNotFoundException, SQLException {
+	public ArrayList<Book> getBooksByCategoryName(String catName) throws ClassNotFoundException, SQLException{
 		Class.forName("org.apache.derby.jdbc.ClientDriver");
 		String urlCn = "jdbc:derby://localhost:1527/LibraryDB";
 		Connection cn = DriverManager.getConnection(urlCn, "administrator", "123456");
+
+		String booksQuery = ""
+			+ "SELECT books.isbn, "
+			+ "       books.title, "
+			+ "       books.author, "
+			+ "       books.p_year, "
+			+ "       books.cover, "
+			+ "       books.copy_cnt, "
+			+ "       Count(book_copies.copy_code) AS usable_copy_count "
+			+ "FROM   books "
+			+ "       LEFT JOIN book_copies "
+			+ "              ON books.isbn = book_copies.isbn "
+			+ "                 AND book_copies.copy_cond != 5 "
+			+ "                 AND book_copies.copy_code NOT IN (SELECT copy_code "
+			+ "                                                   FROM   loaned_books "
+			+ "                                                   WHERE  returned = false) "
+			+ "WHERE  Lower(book_category) = ? "
+			+ "GROUP  BY books.isbn, "
+			+ "          books.title, "
+			+ "          books.author, "
+			+ "          books.p_year, "
+			+ "          books.cover, "
+			+ "          books.copy_cnt";
 
 		ArrayList<Book> books = new ArrayList<Book>();
 		Statement ps = null;
 		try {
 			ps = cn.createStatement();
-			PreparedStatement pst = cn.prepareStatement("select * from books where Lower(book_category)=?");
+//			PreparedStatement pst = cn.prepareStatement("select * from books where Lower(book_category)=?");
+			PreparedStatement pst = cn.prepareStatement(booksQuery);
 			pst.setString(1, catName.toLowerCase());
 			ResultSet rs = pst.executeQuery();
-			while (rs.next()) {
+			while(rs.next()){
 				Book b = new Book();
 				b.setISBN(rs.getString("isbn"));
 				b.setTitle(rs.getString("title"));
@@ -125,10 +150,11 @@ public class CategoryDB {
 				b.setBookYear(Year.parse(rs.getString("p_year"), DateTimeFormatter.ofPattern("yyyy-MM-dd")));
 				b.setCoverPath(rs.getString("cover"));
 				b.setCopyCounter(rs.getInt("copy_cnt"));
+				b.setAvailableCopies(rs.getInt("usable_copy_count"));
 				books.add(b);
 			}
 			cn.close();
-		} catch (SQLException e) {
+		} catch(SQLException e){
 			// TODO
 			// Write an error
 		}
